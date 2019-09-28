781d6f6a6957b93db55978663ec5a0049b685ef5
123300 <sajalislam8988@gmail.com>
2019-09-28 23:52:14 +0600

    textFileAdded

---
+++ Customer.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace CustomerWindows
+{
+    partial class Customer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.showDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.showDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Customer Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Customer Address";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(133, 42);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 3;
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(133, 76);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 4;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(133, 113);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(100, 20);
+            this.addressTextBox.TabIndex = 5;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(44, 186);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(158, 186);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 7;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(267, 186);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 8;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(364, 186);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 9;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(290, 74);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 10;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // showDataGridView
+            // 
+            this.showDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.showDataGridView.Location = new System.Drawing.Point(44, 236);
+            this.showDataGridView.Name = "showDataGridView";
+            this.showDataGridView.Size = new System.Drawing.Size(395, 150);
+            this.showDataGridView.TabIndex = 11;
+            // 
+            // Customer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showDataGridView);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Customer";
+            this.Text = "Customer Information";
+            ((System.ComponentModel.ISupportInitialize)(this.showDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.DataGridView showDataGridView;
+    }
+}
+
---
+++ Customer.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomerWindows
+{
+    public partial class Customer : Form
+    {
+        public Customer()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            //Connection
+
+            string connectionString = @"Server =LAPTOP-JECDIQLU; Database=CustomerInfo; Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            //Command
+            if (!String.IsNullOrEmpty(nameTextBox.Text))
+            {
+                string commandString = @"INSERT INTO Customer(Customer_Name, Customer_address) Values ('" + nameTextBox.Text + "', +'" +addressTextBox.Text + "')";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                sqlConnection.Open();
+
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    MessageBox.Show("add");
+
+                }
+                else
+                {
+                    MessageBox.Show("Not Add");
+                }
+
+                sqlConnection.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please Enter Name and Address");
+                return;
+            }
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            //Connection
+
+            string connectionString = @"Server =LAPTOP-JECDIQLU; Database=CustomerInfo; Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            //Command
+
+            string commandString = @"SELECT * FROM Customer";
+            SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+            sqlConnection.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            if (dataTable.Rows.Count > 0)
+            {
+                showDataGridView.DataSource = dataTable;
+            }
+            else
+            {
+                showDataGridView.DataSource = null;
+                MessageBox.Show("Data not Found!");
+            }
+
+
+
+            sqlConnection.Close();
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            //Connection
+
+            string connectionString = @"Server =LAPTOP-JECDIQLU; Database=CustomerInfo; Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            //Command
+
+            if (!String.IsNullOrEmpty(idTextBox.Text))
+            {
+                string commandString = @"DELETE FROM Customer WHERE ID =" + idTextBox.Text + "";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                sqlConnection.Open();
+
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    MessageBox.Show("Delete");
+                }
+                else
+                {
+                    MessageBox.Show("Not Delete");
+                }
+
+                sqlConnection.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please Enter Id");
+                return;
+            }
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            //Connection
+
+            string connectionString = @"Server =LAPTOP-JECDIQLU; Database=CustomerInfo; Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            //Command
+
+            if (!String.IsNullOrEmpty(idTextBox.Text))
+            {
+                string commandString = @"UPDATE Customer SET Customer_Name = '"+nameTextBox.Text+"' ,Customer_address = '"+addressTextBox.Text+"' WHERE ID = "+idTextBox.Text+"";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                sqlConnection.Open();
+
+                int isExecuted = sqlCommand.ExecuteNonQuery();
+                if (isExecuted > 0)
+                {
+                    MessageBox.Show("Update");
+                }
+                else
+                {
+                    MessageBox.Show("Not Update");
+                }
+
+                sqlConnection.Close();
+            }
+            else
+            {
+                MessageBox.Show("Inter  ID");
+                return;
+            }
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            //Connection
+
+            string connectionString = @"Server =LAPTOP-JECDIQLU; Database=CustomerInfo; Integrated Security=True";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            //Command
+
+            if (!String.IsNullOrEmpty(addressTextBox.Text))
+            {
+                string commandString = @"SELECT Customer_Name, Customer_address FROM Customer WHERE Customer_address ='"+addressTextBox.Text+"'";
+                SqlCommand sqlCommand = new SqlCommand(commandString, sqlConnection);
+
+                sqlConnection.Open();
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                if (dataTable.Rows.Count > 0)
+                {
+                    showDataGridView.DataSource = dataTable;
+                }
+                else
+                {
+                    showDataGridView.DataSource = null;
+                    MessageBox.Show("Data not Found!");
+                }
+
+                sqlConnection.Close();
+            }
+            else
+            {
+                MessageBox.Show("Inter Address");
+                return;
+            }
+        }
+    }
+}
